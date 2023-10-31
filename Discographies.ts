9e89842b3edfbef535620a80b3b779871d7fe306
lectrician1 <jayandseth@gmail.com>
2023-10-31 16:26:41 -0400

    simplify

--- Discographies.ts
+++ Discographies.ts
@@ -1,20 +1,3 @@
-/**
- * THIS IS A COMPILED TYPESCRIPT FILE
- * SEE THE ACTUAL SOURCE CODE AT User:Lectrician1/discographies.js/ts.js
- * 
- * Name: discographies.js
- * Description: Shows useful discography data and functions on 
- *  discography items
- * Note: Only meant to work on page refresh. This is because the 
- *  lag time between creating a statement like "publication date"
- *  and it showing up in BlazeGraph and then by this script is high.
- * Author: Lectrician1
- * License: CC0
- * Functions taken from: 
- *  https://www.wikidata.org/wiki/User:Nikki/ExMusica.js
- *  https://www.wikidata.org/wiki/User:Magnus_Manske/duplicate_item.js
- */
-
 import { SiteData, SiteEntities, WikibaseItem, WikibaseStatement, WikibaseItemProperty, WikibaseDateProperty, WikibaseQuantityProperty, WikibaseMonolingualTextProperty, sparqlQuery, AskQueryResponse } from "./DiscographiesClasses";
 import { showCreateRelease } from './CreateRelease'
 import { startShowDiscography } from './ShowDiscography'
